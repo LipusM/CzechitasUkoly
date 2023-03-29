bb2b5fc7ba12980b3dda6b426e255001b330cd0f
M.Lipus <mlip007@gmail.com>
2023-03-29 18:30:53 +0200

    Úkol za poslední lekci

--- Ukol9/main1.js
+++ Ukol9/main1.js
@@ -8,6 +8,7 @@ let minuty1 = document.querySelector(".alarm__minutes")
 let budik = document.querySelector(".alarm").classList
 let zvoneni = document.querySelector("#zvoneni")
 let resetText = sekundy1.textContent
+let vlozenaHodnota = document.querySelector(".time-input")
 
 let minuty2 = ""
 let sekundy2 = ""
@@ -15,10 +16,11 @@ let sekundy2 = ""
 function spustOdpocet(e){
     e.preventDefault()
 
-    time = Number(document.querySelector(".time-input").value)
+    time = Number(vlozenaHodnota.value.replace(/\D/g, ''))
     odpocet1 = setInterval(odpocitavani, 1000)
 
     budik.remove("alarm--ring")
+    obarveni()
 }
 
 function odpocitavani(){
@@ -55,6 +57,14 @@ function resetOdpocet(e){
     zvoneni.currentTime = 0
 
     budik.remove("alarm--ring")
+}
+
+ function obarveni(){
+    if (time === time){
+        vlozenaHodnota.classList.add("spravneObarveni") //Proč mi nejde přidat tato hodnota?
+        /* vlozenaHodnota.style.borderColor = "green" */ //Ani toto nejde
+        c("ano") //Podmínka je splněna, jelikož se to do konzole vypíše.
+    }
 }
 
 
